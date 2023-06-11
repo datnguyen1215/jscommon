494550079e1c7ac9a5ffcc00b579870c78e8985d
Dat Nguyen <datnguyen1215@gmail.com>
2023-06-11 13:00:10 -0700

    Add EventEmitter and its test cases.

---
+++ src/events.js
@@ -0,0 +1,62 @@
+class EventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  /**
+   * Subscribe to an event.
+   * @param {string} type
+   * @param {Function} listener
+   * @return {void}
+   */
+  on(type, listener) {
+    this.events[type] = this.events[type] || [];
+    this.events[type].push(listener);
+  }
+
+  /**
+   * Emit an event.
+   * @param {string} type
+   * @param {any} args
+   */
+  emit(type, ...args) {
+    if (!this.events[type]) return;
+    this.events[type].forEach(listener => listener(...args));
+  }
+
+  /**
+   * Remove an event listener.
+   * @param {string} type
+   * @param {Function} listener
+   */
+  off(type, listener) {
+    if (!this.events[type]) return;
+    this.events[type] = this.events[type].filter(x => x !== listener);
+  }
+
+  /**
+   * Subscribe to an event once.
+   * @param {string} type
+   * @param {Function} listener
+   */
+  once(type, listener) {
+    const onceListener = (...args) => {
+      listener(...args);
+      this.off(type, onceListener);
+    };
+    this.on(type, onceListener);
+  }
+
+  /**
+   * Remove all listeners for an event.
+   * @param {string} type
+   */
+  removeAllListeners(type) {
+    if (!this.events[type]) return;
+    this.events[type] = [];
+  }
+}
+
+export { EventEmitter };
+
+export default EventEmitter;
---
+++ src/events.test.js
@@ -0,0 +1,72 @@
+import { EventEmitter } from 'events';
+import events from 'events';
+
+import assert from 'assert';
+
+describe('events', () => {
+  it('should work with import default and named import', done => {
+    assert(events === EventEmitter);
+    done();
+  });
+
+  it('on() should be called multiple times', done => {
+    const emitter = new EventEmitter();
+
+    let count = 0;
+
+    const onEvent = () => count++;
+    emitter.on('event', onEvent);
+
+    emitter.emit('event');
+    emitter.emit('event');
+    emitter.emit('event');
+
+    assert(count === 3);
+    emitter.off('event', onEvent);
+    done();
+  });
+
+  it('off() should remove listener', done => {
+    const emitter = new EventEmitter();
+
+    let count = 0;
+    const listener = () => count++;
+    emitter.on('event', listener);
+    emitter.emit('event');
+    emitter.off('event', listener);
+    emitter.emit('event');
+
+    assert(count === 1);
+    done();
+  });
+
+  it('once() should be called only once', done => {
+    const emitter = new EventEmitter();
+
+    let count = 0;
+    emitter.once('event', () => count++);
+
+    emitter.emit('event');
+    emitter.emit('event');
+    emitter.emit('event');
+
+    assert(count === 1);
+    done();
+  });
+
+  it('removeAllListeners() should remove all listeners', done => {
+    const emitter = new EventEmitter();
+
+    let count = 0;
+    emitter.on('event', () => count++);
+    emitter.on('event', () => count++);
+
+    emitter.removeAllListeners('event');
+    emitter.emit('event');
+    emitter.emit('event');
+    emitter.emit('event');
+
+    assert(count === 0);
+    done();
+  });
+});
