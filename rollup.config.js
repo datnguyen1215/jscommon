4bde0c6923c824419a42f4f5efa6f02b881fbdca
Dat Nguyen <datnguyen1215@gmail.com>
2023-06-14 18:17:18 -0700

    Add build scripts.

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import { babel } from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+export default {
+  input: {
+    events: 'src/events.js',
+    utils: 'src/utils.js',
+    hsm: 'src/hsm/index.js',
+    websocket: 'src/websocket/index.js',
+    index: 'src/index.js'
+  },
+
+  output: [
+    {
+      dir: 'dist',
+      format: 'cjs', // CommonJS module format
+      entryFileNames: '[name].js',
+      exports: 'named'
+    }
+  ],
+
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    babel({ babelHelpers: 'bundled' })
+  ]
+};
