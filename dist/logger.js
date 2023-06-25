22c8089e4d09c422d1c852df264de3cac607ccce
Dat Nguyen <datnguyen1215@gmail.com>
2023-06-25 01:22:24 -0700

    Expose logger.format.colorize().

--- dist/logger.js
+++ dist/logger.js
@@ -294,10 +294,40 @@ const timestamp = (options = DEFAULT_OPTIONS) => {
   });
 };
 
+const DEFAULT_COLORS = {
+  debug: '\x1b[34m',
+  info: '\x1b[32m',
+  warn: '\x1b[33m',
+  error: '\x1b[31m',
+  fatal: '\x1b[35m'
+};
+
+/**
+ * Colorize the log messae.
+ * NOTE: This should only be used for console logs.
+ *
+ * @param {ColorizeOptions} options
+ * @returns {Format}
+ */
+const colorize = (options = {}) => {
+  const colors = {
+    ...DEFAULT_COLORS,
+    ...options.colors
+  };
+  return info => {
+    const color = colors[info.level];
+    return {
+      ...info,
+      message: `${color}${info.message}\x1b[0m`
+    };
+  };
+};
+
 const index$1 = {
   combine,
   custom,
-  timestamp
+  timestamp,
+  colorize
 };
 
 const levels = {
--- src/logger/format/index.js
+++ src/logger/format/index.js
@@ -1,6 +1,7 @@
 import combine from './combine';
 import custom from './custom';
 import timestamp from './timestamp';
+import colorize from './colorize';
 
-const index = { combine, custom, timestamp };
-export { combine, custom, timestamp, index as default };
+const index = { combine, custom, timestamp, colorize };
+export { combine, custom, timestamp, colorize, index as default };
